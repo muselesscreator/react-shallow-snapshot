5a55f1087c867e1d21676581f4b571d1aca917f8
muselesscreator <bwarzesk@gmail.com>
2024-03-06 14:13:09 -0500

    feat: add mockName util to exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 /* v8 ignore start */
 export { default as formatMessage } from './formatMessage';
 export { default as mockComponents } from './mockComponents';
+export { default as setMockName } from './setMockName';
 export { default as shallow } from './shallow';
