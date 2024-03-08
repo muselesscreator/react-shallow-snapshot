6bbc1b2aa97095e6ad34378aa6282f0ea91d22a7
muselesscreator <bwarzesk@gmail.com>
2024-03-07 23:17:16 -0500

    chore: more type exports

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { default as formatMessage } from './formatMessage';
 export { default as mockComponents } from './mockComponents';
 export { default as setMockName } from './setMockName';
 export { default as shallow, ReactShallowRenderer } from './shallow';
+export type { ExplorerData } from './types';
